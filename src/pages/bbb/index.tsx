63b46065d26787d69cafcfa8b95c7dab8b2aedfe
qinzhen <qz519189636@qq.com>
2021-12-14 18:02:06 +0800

    feat: test video

--- src/pages/bbb/index.tsx
+++ src/pages/bbb/index.tsx
@@ -1,35 +1,85 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import videojs, { VideoJsPlayer } from "video.js";
+import { Parser } from "m3u8-parser";
 import "video.js/dist/video-js.min.css";
+
+const src =
+  "https://test-1302380933.cos.ap-shanghai.myqcloud.com/3b70b7e222488ffca5a464bfc6b7cff7_xxx.m3u8";
 
 export const BComponent = () => {
   const videoRef = useRef(null);
   const playerRef = useRef<VideoJsPlayer>();
+  const [segmentsLen, setSegmentsLen] = useState(0);
+  const [hasChangePlayList, setHasChangePlayList] = useState(false);
+
+  let player: VideoJsPlayer = null as unknown as VideoJsPlayer;
 
   const options = {
     controls: true,
     // liveui: true,
   };
 
+  const m3u8Analysis = async () => {
+    var parser = new Parser();
+    return fetch(src)
+      .then((response) => response.text())
+      .then((body) => {
+        parser.push(body);
+        parser.end();
+        const parsedManifest = parser.manifest;
+        // console.log("body", body);
+        // console.log("parsedManifest", parsedManifest);
+        // debugger;
+        return parsedManifest;
+      });
+  };
+
+  const rePlay = () => {
+    debugger;
+    player.reset();
+    player.src({
+      type: "application/x-mpegURL",
+      src,
+    });
+    player.load();
+    player.play();
+  };
+
+  const ended = () => {
+    console.log("ended ended ended ended");
+    const whereYouAt = player.currentTime();
+    const lengthOfVideo = player.duration();
+    console.log("whereYouAt", whereYouAt);
+    console.log("lengthOfVideo", lengthOfVideo);
+  };
+
+  const ready = async () => {
+    console.log("player is ready");
+    player.autoplay(true);
+    player.src({
+      type: "application/x-mpegURL",
+      src,
+    });
+
+    player.on("ended", ended);
+
+    const data = await m3u8Analysis();
+    setSegmentsLen(data?.segments?.length || 0);
+
+    // player.on("timeupdate", (event) => {
+    //   console.log("The currentTime attribute has been updated. Again.", event);
+    // });
+
+    // player.on("durationchange", (event) => {
+    //   console.log("Not sure why, but the duration of the video has changed.", event);
+    // });
+  };
+
   useEffect(() => {
-    // make sure Video.js player is only initialized once
     if (!playerRef.current) {
       const videoElement = videoRef.current;
       if (!videoElement) return;
-      const player = (playerRef.current = videojs(videoElement, options, () => {
-        console.log("player is ready");
-        player.autoplay(true);
-        player.src({
-          type: "application/x-mpegURL",
-          src: "https://test-1302380933.cos.ap-shanghai.myqcloud.com/3b70b7e222488ffca5a464bfc6b7cff7_xxx.m3u8",
-        });
-      }));
-    } else {
-      // debugger;
-      // const player = playerRef.current;
-      // player.autoplay(true);
-      // player.src("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
-      // debugger;
+      player = playerRef.current = videojs(videoElement, options, ready);
     }
   }, [videoRef]);
 
@@ -46,8 +96,12 @@ export const BComponent = () => {
   }, [playerRef]);
 
   return (
-    <div data-vjs-player>
-      <video ref={videoRef} className="video-js vjs-big-play-centered" />
+    <div>
+      <div>当前segments长度: {segmentsLen}</div>
+      {hasChangePlayList ? <div>PlayList 发生了改变</div> : null}
+      <div data-vjs-player>
+        <video ref={videoRef} className="video-js vjs-big-play-centered" />
+      </div>
     </div>
   );
 };
