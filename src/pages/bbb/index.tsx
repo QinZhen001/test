16d4c6f1f031a173a2b57fd6760d3e1bc7502474
qinzhen <qz519189636@qq.com>
2021-12-21 18:26:28 +0800

    qz commit

--- src/pages/bbb/index.tsx
+++ src/pages/bbb/index.tsx
@@ -6,18 +6,39 @@ import "video.js/dist/video-js.min.css";
 const src =
   "https://test-1302380933.cos.ap-shanghai.myqcloud.com/3b70b7e222488ffca5a464bfc6b7cff7_xxx.m3u8";
 
+let segmentsLen = 0;
+
+const options = {
+  controls: true,
+  // liveui: true,
+};
+
 export const BComponent = () => {
   const videoRef = useRef(null);
+  const videoRef2 = useRef(null);
   const playerRef = useRef<VideoJsPlayer>();
-  const [segmentsLen, setSegmentsLen] = useState(0);
+  const playerRef2 = useRef<VideoJsPlayer>();
+  const [show, setShow] = useState(true);
   const [hasChangePlayList, setHasChangePlayList] = useState(false);
 
   let player: VideoJsPlayer = null as unknown as VideoJsPlayer;
+  let player2: VideoJsPlayer = null as unknown as VideoJsPlayer;
 
-  const options = {
-    controls: true,
-    // liveui: true,
-  };
+  useEffect(() => {
+    if (!playerRef.current) {
+      const videoElement = videoRef.current;
+      if (!videoElement) return;
+      player = playerRef.current = videojs(videoElement, options, ready);
+    }
+  }, [videoRef]);
+
+  useEffect(() => {
+    if (!playerRef2.current) {
+      const videoElement = videoRef2.current;
+      if (!videoElement) return;
+      player2 = playerRef2.current = videojs(videoElement, options, ready2);
+    }
+  }, [videoRef]);
 
   const m3u8Analysis = async () => {
     var parser = new Parser();
@@ -35,22 +56,27 @@ export const BComponent = () => {
   };
 
   const rePlay = () => {
-    debugger;
-    player.reset();
-    player.src({
-      type: "application/x-mpegURL",
-      src,
-    });
-    player.load();
-    player.play();
+    // debugger;
+    // player.reset();
+    // player.src({
+    //   type: "application/x-mpegURL",
+    //   src,
+    // });
+    // player.load();
+    // player.play();
   };
 
-  const ended = () => {
+  const ended = async () => {
     console.log("ended ended ended ended");
     const whereYouAt = player.currentTime();
     const lengthOfVideo = player.duration();
     console.log("whereYouAt", whereYouAt);
     console.log("lengthOfVideo", lengthOfVideo);
+    const data = await m3u8Analysis();
+    if (data.segments.length !== segmentsLen) {
+      segmentsLen = data.segments.length;
+      rePlay();
+    }
   };
 
   const ready = async () => {
@@ -64,7 +90,7 @@ export const BComponent = () => {
     player.on("ended", ended);
 
     const data = await m3u8Analysis();
-    setSegmentsLen(data?.segments?.length || 0);
+    segmentsLen = data.segments.length;
 
     // player.on("timeupdate", (event) => {
     //   console.log("The currentTime attribute has been updated. Again.", event);
@@ -75,32 +101,41 @@ export const BComponent = () => {
     // });
   };
 
-  useEffect(() => {
-    if (!playerRef.current) {
-      const videoElement = videoRef.current;
-      if (!videoElement) return;
-      player = playerRef.current = videojs(videoElement, options, ready);
-    }
-  }, [videoRef]);
+  const ready2 = () => {
+    player2.src({
+      type: "application/x-mpegURL",
+      src,
+    });
+  };
+
+  const exchange = () => {
+    setShow((v) => !v);
+  };
 
   // Dispose the Video.js player when the functional component unmounts
-  useEffect(() => {
-    const player = playerRef.current;
+  //   useEffect(() => {
+  //     const player = playerRef.current;
 
-    return () => {
-      if (player) {
-        player.dispose();
-        playerRef.current = undefined;
-      }
-    };
-  }, [playerRef]);
+  //     return () => {
+  //       if (player) {
+  //         player.dispose();
+  //         playerRef.current = undefined;
+  //       }
+  //     };
+  //   }, [playerRef]);
 
   return (
     <div>
+      <button className="exchange" onClick={exchange}>
+        exchange
+      </button>
       <div>当前segments长度: {segmentsLen}</div>
       {hasChangePlayList ? <div>PlayList 发生了改变</div> : null}
-      <div data-vjs-player>
+      <div className="wrapper wrapper1" style={{}}>
         <video ref={videoRef} className="video-js vjs-big-play-centered" />
+      </div>
+      <div className="wrapper wrapper2" style={{}}>
+        <video ref={videoRef2} className="video-js vjs-big-play-centered" />
       </div>
     </div>
   );
