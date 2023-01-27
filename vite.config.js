3bb7477ac7973878d5cff2fc54f4ae9e66724442
qinzhen <qz519189636@qq.com>
2023-01-27 23:11:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve:{
     alias:{
       'vue': 'vue/dist/vue.esm-bundler.js'
