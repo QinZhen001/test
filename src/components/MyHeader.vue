8fc55b18c3dd5051795a7d45591545835da852a8
qinzhen <qz519189636@qq.com>
2023-01-27 23:24:10 +0800

    Update MyHeader.vue

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,11 +1,18 @@
 // just for my plugin
 <template>
-  <div v-font-size:small>header {{mixinUrl}} <button @click="logout">logout</button></div>
+  <section class="component">
+    <div>plugin register global component</div>
+    <div v-font-size:small>header with plugin custom directive v-font-size</div>
+    <div>plugin mixin data mixinUrl:{{ mixinUrl }} </div>
+    <div>
+      <button @click="logout">logout (inject)</button>
+    </div>
+  </section>
 </template>
 
 
 <script  setup>
-import { ref,inject } from 'vue'
+import { ref, inject } from 'vue'
 
 const logout = inject('logout')
 
